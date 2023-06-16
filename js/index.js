a71948d75ec1e6500a24c73f67d28d63ba91e568
Oumou Coulibaly <coumou100@gmail.com>
2023-06-16 13:06:01 +0000

    examen jquery

---
+++ js/index.js
@@ -0,0 +1,119 @@
+$('body').css('font-family', 'Open Sans')
+    .css("padding", "0")
+    .css('margin', "0");
+$("nav").css("display", 'flex')
+    .css('justify-content', 'space-between')
+    .css('color', 'white')
+    .css("background-color", '#656565')
+    .css('padding', '50px');
+
+    ;
+
+$("nav .logo").css("margin-top", '15px')
+    .css('color', "#da491c");
+
+$("nav ul").css("display", 'flex');
+$("nav ul li").css("margin-right", '15px')
+    .css('margin-left', "15px")
+    .css('list-style-type', 'none')
+    .css('font-size', '20px');
+
+$("nav ul li a").css("color", 'white')
+    .css('text-decoration', 'none');
+
+$("nav ul .current").css('color', '#da491c')
+
+$("footer").css("background-color", "#292929")
+    .css('color', 'white');
+
+$('.firstSection').css("background-color", '#656565')
+    .css('color', 'white')
+    .css('text-align', 'center')
+    .css("display", "flex")
+    .css('flex-direction', 'column')
+    .css('justify-content', 'center')
+    .css('align-items', 'center')
+    .css('padding', '50px')
+    .css('padding', '50px');
+
+$('.firstSection h1 span').css("color", '#da491c');
+$('.firstSection h1').css("border-bottom", '2px solid #da491c')
+    .css("width", "fit-content");
+
+$('.firstSection p').css("font-size", '22px');
+
+
+$('.secondSection').css('display', 'flex')
+    .css('flex-wrap', 'wrap')
+    .css('justify-content', 'center')
+    .css('background-color', '#F0EEEE')
+    .css('padding', '50px');
+;
+
+$('.secondSection div').css('width', '500px')
+    .css('margin', '20px');
+
+
+$('.thirdSection h2').css('color', '#da491c')
+    .css('padding', '5px')
+    .css('border-left', "3px solid ");
+
+$('.thirdSection p').css('font-size', '20px');
+$('.thirdSection').css('margin-top', '50px')
+    .css('padding', '50px');
+;
+$('.thirdSection .img').css('display', 'flex')
+    .css('flex-wrap', 'wrap')
+    .css('justify-content', 'center')
+    .css('margin', '50px');
+$('.thirdSection .img div').css('background-color', 'black')
+    .css('width', '300px')
+    .css('height', '300px')
+    .css('margin', '5px');
+
+$(".thirdSection img").css('width', '200px')
+    .css('height', '100px')
+    .css('margin-top', '100px')
+    .css('margin-left', '50px');
+
+$('.thirdSection1 h2').css('color', '#da491c')
+    .css('padding', '5px')
+    .css('border-left', "3px solid ");
+
+
+$(".thirdSection1 img").css('margin', '20px');
+$(".thirdSection1 ").css('padding', '50px');
+
+
+$('.thirdSection1 .img1').css('background-color', '#656565')
+    .css('width', 'fit-content')
+    .css('margin-left', '50px')
+    .css('margin-bottom', '50px');
+;
+
+$('.fourthSection').css('background-color', '#F0EEEE')
+    .css('display', 'flex')
+    .css('flex-direction', 'column')
+    .css('justify-content', 'center')
+    .css('align-items', 'center')
+    .css('padding', '100px');
+
+$('.fourthSection1').css('background-color', '#F0EEEE')
+    .css('padding', '50px')
+$(".fourthSection1 button").css('color', '#fff')
+    .css('background-color', '#da491c')
+    .css('padding', '10px')
+    .css('border', 'none')
+    .css('border-radius', '5px');
+
+
+$('.fourthSection span').css('color', '#da491c');
+
+$('footer').css('padding', '50px');
+$('footer .contact .email').css('border-bottom', "solid 2px #F0EEEE");
+$('footer .contact .news').css('font-size', '13px')
+    .css('margin-top', '20px');
+
+$('footer span').css('margin-top', '50px');
+
+$('footer .RS').css('float', 'right')
